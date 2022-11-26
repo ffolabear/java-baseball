75cd236218e56df1d4b904d57718c79f1b129b88
ffolabear <ffolabear@gmail.com>
2022-11-26 17:32:31 +0900

    feat(Application): 어플리케이션 시작하는 부분 작성

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,5 +1,7 @@
 package baseball;
 
+import baseball.service.Game;
+
 /**
  * 빠르게 구현하기 스터디 브랜치
  */
@@ -7,5 +9,7 @@ public class Application {
 
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        Game game = new Game();
+        game.startGame();
     }
 }
