5bc5b12633e606aef4bae64f4efa2a1015b2ffd3
ffolabear <ffolabear@gmail.com>
2022-11-28 02:28:25 +0900

    test(GameTest): 게임 기능 테스트 코드 작성

--- src/test/java/baseball/GameTest.java
+++ src/test/java/baseball/GameTest.java
@@ -12,9 +12,19 @@ public class GameTest extends NsTest {
     void 낫싱_테스트() {
         assertRandomNumberInRangeTest(
                 () -> {
-                    run("123");
-                    assertThat(output()).contains("");
-                } , 4,5,6
+                    run("123", "456", "2");
+                    assertThat(output()).contains("낫싱");
+                }, 4, 5, 6
+        );
+    }
+
+    @Test
+    void 스트라이크_테스트() {
+        assertRandomNumberInRangeTest(
+                () -> {
+                    run("546", "456", "2");
+                    assertThat(output()).contains("2볼 1스트라이크");
+                }, 4, 5, 6
         );
     }
 
